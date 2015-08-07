becd49e9288bd52202360b7d56bd588e3725b100
Juan David Pastás Rivera <juandavid1707@gmail.com>
2015-08-06 20:59:33 -0500

    no bug

--- has_one.rb
+++ has_one.rb
@@ -1,16 +1,17 @@
 begin
-  require 'bundler/inline'
+	require 'bundler/inline'
 rescue LoadError => e
-  $stderr.puts 'Bundler version 1.10 or later is required. Please update your Bundler'
-  raise e
+	$stderr.puts 'Bundler version 1.10 or later is required. Please update your Bundler'
+	raise e
 end
 
 gemfile(true) do
-  source 'https://rubygems.org'
-  # Activate the gem you are reporting the issue against.
-  gem 'activerecord', '4.2.0'
-  gem 'sqlite3'
-  gem 'goldiloader'
+	source 'https://rubygems.org'
+	# Activate the gem you are reporting the issue against.
+	gem 'activerecord', '4.2.0'
+	gem 'sqlite3'
+	gem 'goldiloader'
+	gem 'pry'
 end
 
 require 'active_record'
@@ -25,29 +26,63 @@ ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:'
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 
 ActiveRecord::Schema.define do
-  create_table :posts, force: true do |t|
-  end
+	create_table :posts, force: true do |t|
 
-  create_table :comments, force: true do |t|
-    t.integer :post_id
-  end
+	end
+
+	create_table :things, force: true do |t|
+		t.integer :post_id
+	end
+
+	create_table :comments, force: true do |t|
+		t.integer :post_id
+	end
 end
 
 class Post < ActiveRecord::Base
-  has_many :comments
+	has_many :comments
+	has_one :thing
 end
 
 class Comment < ActiveRecord::Base
-  belongs_to :post
+	belongs_to :post
+	has_one :thing, through: :post
+end
+
+class Thing < ActiveRecord::Base
+	belongs_to :post
+	has_many :comments, through: :post
 end
 
 class BugTest < Minitest::Test
-  def test_association_stuff
-    post = Post.create!
-    post.comments << Comment.create!
+	def test_association_stuff
+		post = Post.create!
+		post1 = Post.create!
+		post.comments << Comment.create!
+		post.comments << Comment.create!
+		post1.comments << Comment.create!
+		Post.all.each do |post|
+			Thing.create post: post
+		end
+		binding.pry
 
-    assert_equal 1, post.comments.count
-    assert_equal 1, Comment.count
-    assert_equal post.id, Comment.first.post.id
-  end
+
+		# assert_equal 2, post.comments.count
+		# assert_equal 3, Comment.count
+		# assert_equal post.id, Comment.first.post.id
+
+
+
+		Comment.all.each do |c|
+			p c.post.id
+			p c.thing.id
+		end
+
+		# Post.all.each do |post|
+		# 	post.comments.each do |c|
+		# 		p c.id
+		# 	end
+		# 	post.thing.id
+		# end
+	end
 end
